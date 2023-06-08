605a3c54af77730cf1e49f81c65b53351f1d6171
KenanHasanzade <kenanhsn034@gmail.com>
2023-06-08 14:59:19 +0400

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,43 @@
+window.addEventListener('DOMContentLoaded',()=>{
+const tabs = document.querySelectorAll('.tabheader__item'),
+      content=document.querySelectorAll('.tabcontent'),
+      tabsParent = document.querySelector('.tabheader__items')
+
+
+
+function hideTabContent(){
+    content.forEach(item =>{
+        item.classList.add('hide');
+        item.classList.remove('show','fade');
+    })
+    tabs.forEach(item =>{
+        item.classList.remove('tabheader__item_active')
+    })
+}
+
+function showTabContent(i = 0){
+    content[i].classList.add('show','fade');
+    content[i].classList.remove('hide');
+    tabs[i].classList.add('tabheader__item_active')
+}
+
+
+
+hideTabContent();
+showTabContent()
+tabsParent.addEventListener('click',(event)=>{
+   const target = event.target;
+
+   if(target && target.classList.contains('tabheader__item')){
+    
+    
+    tabs.forEach((item,i)=>{
+      if(target == item){
+        hideTabContent();
+        showTabContent(i);
+      }
+    })
+   }
+})
+   
+})
